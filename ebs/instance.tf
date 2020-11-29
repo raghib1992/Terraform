6070da13e13a644e7a70419ae797b52edb31f500
raghib1992 <raghib.npti@gmail.com>
2020-11-30 04:04:37 +0530

    update ebs

--- ebs/instance.tf
+++ ebs/instance.tf
@@ -5,22 +5,16 @@ resource "aws_key_pair" "mykey" {
 
 resource "aws_instance" "test_ebs" {
     ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-    instance_type = "${var.INSTANCE}"
-    # VPC subnet
-    # subnet_id = "value"
-    tenancy = "default"
+    instance_type = "t2.micro"
+    availability_zone = "${var.AWS_REGION}a"
     key_name = aws_key_pair.mykey.key_name
-    # security group
-    # vpc_security_group_ids = ""
-    # user dat
-    # user_data = ""
     tags = {
         Name = "test_ebs"
     }
 }
 
 resource "aws_ebs_volume" "volume-1" {
-  availability_zone = "${var.AWS_REGION}"
+  availability_zone = "${var.AWS_REGION}a"
   size              = 40
   type = "gp2"
   tags = {
--- ebs/var.tf
+++ ebs/var.tf
@@ -10,12 +10,7 @@ variable "AMIS" {
         eu-north-1 = "ami-0a3a4169ad7cb0d77"
         ap-south-1 = "ami-0a4a70bd98c6d6441"
         us-west-2 = "ami-07dd19a7900a1f049"
-        us-east-1 = "ami-0885b1f6bd170450c"
     }
-}
-
-variable "INSTANCE" {
-    default = "t2.micro"
 }
 
 variable "AWS_ACCESS_KEY" {}
