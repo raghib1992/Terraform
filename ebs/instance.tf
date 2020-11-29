cc33ae78a2b9f00e0e9d514d28acfa7c1a8d1f36
raghib1992 <raghib.npti@gmail.com>
2020-11-30 04:11:47 +0530

    update ebs

--- ebs/instance.tf
+++ ebs/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "mykey" {
     key_name = "mykey"
-    public_key = "${var.PUBLIC_KEY}"  
+    public_key = "${file(var.PUBLIC_KEY)}"  
 }
 
 resource "aws_instance" "test_ebs" {
