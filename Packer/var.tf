5bb8d2053a6671ca2faae947d62739d8169ae9d5
Ubuntu <ubuntu@ip-10-0-1-83.ap-south-1.compute.internal>
2020-12-10 15:45:55 +0000

    modify packer

--- Packer/var.tf
+++ Packer/var.tf
@@ -9,3 +9,7 @@ variable "PATH_TO_PRIVATE_KEY" {
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
+
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
--- iam/var.tf
+++
@@ -1,5 +0,0 @@
-variable "AWS_REGION" {}
-
-variable "AWS_ACCESS_KEY" {}
-
-variable "AWS_SECRET_KEY" {}
