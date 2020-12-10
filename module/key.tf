817b39057aeaea85ca7d240d1bc357d1f2a369f4
raghib1992 <raghib.npti@gmail.com>
2020-12-10 14:44:45 +0530

    update module and packer folder

--- module/key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "insert_key" {
-    key_name = "remote_key"
-    public_key = "${file(var.PUBLIC_KEY)}"  
-}
--- module/var.tf
+++ module/var.tf
@@ -1,11 +1,2 @@
-variable "AWS_REGION" {
-    default = "us-west-2"  
-}
-
-variable "PUBLIC_KEY" {
-    default = "remote_key.pub"
-}
-
-variable "PRIVATE_KEY" {
-    default = "remote_key"  
-}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
