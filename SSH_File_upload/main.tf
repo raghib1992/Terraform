35802a52fa7c3a05b5a5d0b69078cc237f60bcf9
raghib1992 <raghib.npti@gmail.com>
2021-03-10 16:18:45 +0530

    adding chnages in ssh file

--- SSH_File_upload/main.tf
+++ SSH_File_upload/main.tf
@@ -1,12 +1,12 @@
-resource "aws_key_pair" "keypair" {
-    key_name = "ssh_key"
+resource "aws_key_pair" "mykey" {
+    key_name = "test-key"
     public_key = "${file(var.PUBLIC_KEY)}"
 }
 
-resource "aws_instance" "ec2_test" {
-  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+resource "aws_instance" "ec2_terra" {
+  ami = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.keypair.key_name}"
+  key_name = aws_key_pair.mykey.key_name
 
   provisioner "file" {
     source = "script.sh"
@@ -21,7 +21,7 @@ resource "aws_instance" "ec2_test" {
   }
 
   connection {
-    user = "${lookup(var.USER, var.AWS_REGION)}"
+    user = var.USERNAME
     private_key = "${file("${var.PRIVATE_KEY}")}"
     host = coalesce(self.public_ip, self.private_ip)
   }
