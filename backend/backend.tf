39c2e582056c9f2821afe80080d23fee0cce91a2
raghib1992 <raghib.npti@gmail.com>
2021-03-26 19:47:28 +0530

    remove

--- backend/backend.tf
+++ backend/backend.tf
@@ -3,8 +3,6 @@ terraform {
         bucket = "shaheen-balti-1"  # provide the s3 bucket name
         key = "tarraform/backup"
         region = "ap-south-1"
-        access_key = "AKIAYH7GG6GJIBRBKZSH"
-        secret_key = "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"
     }
 }
 
